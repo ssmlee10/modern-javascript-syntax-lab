42705046c5edd8d7677ce939bb7d6f73cbfa512e
Sharon Lee <ssmlee10@gmail.com>
2025-08-06 13:53:17 -0700

    complete exercise 8

--- app.js
+++ app.js
@@ -113,4 +113,19 @@ userProfile[propertyName] = 'user@gmail.com';
 
 console.log(userProfile);
 
- 
+// ! Exercise 8:
+// a. Create a function with two parameters, `noun` and `adjective`.
+// 
+// b. Give `noun` a default value of "cat" and `adjective` a default value of 
+//    "orange".
+//
+// c. The function should log a sentence 'The cat is orange.' by default, but 
+//    should substitute the appropriate parameters when it is supplied with 
+//    arguments.
+//
+// Your code here:
+
+function sentence(noun = 'cat', adjective = 'orange') {
+    return `The ${noun} is ${adjective}.`;
+};
+console.log(sentence('bear'));
