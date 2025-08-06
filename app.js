0c6854b3d1abeeafbda61f924ca3cac1582e6f12
Sharon Lee <ssmlee10@gmail.com>
2025-08-06 12:43:25 -0700

    complete exercise 5

--- app.js
+++ app.js
@@ -69,3 +69,29 @@ const morePizzaToppings = ['Cheese', 'Sauce'];
 
 const uncontroversialPizzaToppings = [...morePizzaToppings];
 console.log(uncontroversialPizzaToppings);
+
+// ! Exercise 5:
+// a. Duplicate the provided `anotherCar` object and spread its values into a 
+//    new variable named `myCar`.
+//
+// b. Change the `make` and `model` properties of the `myCar` object to new 
+//    values.
+//
+// c. Console log both objects and observe the results.
+//
+// Starting code (don't modify this):
+
+const anotherCar = {
+  make: 'Toyota',
+  model: 'RAV4',
+};
+
+// Your code here:
+const myCar = {...anotherCar};
+myCar.make = 'Toyota';
+myCar.model = 'Camry';
+
+console.log(anotherCar);
+console.log(myCar);
+
+
